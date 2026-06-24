cd321542898e5ef63b6668aaaab58d02678484e7
Waeil55 <waeil.usa@gmail.com>
2026-06-23 21:11:08 -0400

    Reports: move overflow-x-auto to main container so thead sticky works relative to toolbar

--- src/components/ReportsPage.jsx
+++ src/components/ReportsPage.jsx
@@ -953,7 +953,7 @@ const ReportsPage = ({ trips = [], drivers = [], vehicles = [], driverTelemetry 
       )}
 
       {/* Table Content */}
-      <div className="flex-1 min-h-0 overflow-y-auto">
+      <div className="flex-1 min-h-0 overflow-y-auto overflow-x-auto">
         {paginatedGroupedTrips.length === 0 ? (
           <div className="flex flex-col items-center justify-center h-full text-slate-400">
             <FileText size={40} className="mb-3 opacity-40" />
@@ -1025,7 +1025,7 @@ const ReportsPage = ({ trips = [], drivers = [], vehicles = [], driverTelemetry 
                 {!isCollapsed && (
                   <>
                     {/* Table */}
-                    <div className="w-full overflow-x-auto">
+                    <div>
                   <table className="resizable-table text-xs w-full">
                     <colgroup>
                       <col style={{ width: 48 }} />
