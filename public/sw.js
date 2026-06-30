18396becfd6b71b7791f0d145244b82f4b6ca6b8
Waeil55 <waeil.usa@gmail.com>
2026-06-30 19:50:35 -0400

    Bump SW cache version v14->v15 to force cache bust

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@
    Firestore onSnapshot listeners own all realtime data delivery.
 */
 
-const CACHE_VERSION = 'agape-v14';
+const CACHE_VERSION = 'agape-v15';
 const RUNTIME_CACHE = CACHE_VERSION + '-assets';
 
 self.addEventListener('install', (event) => {
