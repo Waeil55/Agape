8402a63a9c2d990240f10aa105ec14e94012ad6c
Waeil55 <waeil.usa@gmail.com>
2026-08-12 15:55:13 -0400

    Bump SW cache to v34 for premium design system

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@
    Firestore onSnapshot listeners own all realtime data delivery.
 */
 
-const CACHE_VERSION = 'agape-v33';
+const CACHE_VERSION = 'agape-v34';
 const RUNTIME_CACHE = CACHE_VERSION + '-assets';
 const CORE_ASSETS = ['/index.html', '/manifest.webmanifest', '/agape.png', '/agape.svg'];
 
